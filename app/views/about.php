cf30d245b0c39e01137292350b3afdac1b7cd421
Alexandr Varygin <otz-job@mail.ru>
2019-12-30 11:09:21 +0300

    start use component for view -> league/plates (only for example)

---
+++ app/views/about.php
@@ -0,0 +1,4 @@
+<?php $this->layout('layout', ['title' => 'User Profile']) ?>
+
+<h1>About Page</h1>
+<p><?= $this->e($title);?></p>
--- public/index.php
+++ public/index.php
@@ -2,20 +2,10 @@
 
 require "../vendor/autoload.php";
 
+// Create new Plates instance
+$templates = new League\Plates\Engine('../app/views');
 
-
-//use App\QueryBuilder;
-//
-//$db = new QueryBuilder();
-//
-//var_dump($db);
-
-
-
-if ($_SERVER["REQUEST_URI"] == "/home") {
-    require "../app/controllers/homepage.php";
-}
-
-exit;
+// Render a template
+echo $templates->render('about', ['title' => 'Jonathan']);
 
 ?>
