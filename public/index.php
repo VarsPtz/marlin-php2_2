e09ba6d2ef3b9ef5942f5ef99b47f018108c4f71
Alexandr Varygin <otz-job@mail.ru>
2020-01-04 22:31:18 +0300

    try to use tamtamchik-simple-flash, t-s-f need bootstrap)

--- public/index.php
+++ public/index.php
@@ -1,20 +1,37 @@
 <?php
+    //Start a Session
+    if( !session_id() ) @session_start();
 
-require "../vendor/autoload.php";
-
-//аналог var_dump()
-//d([123]);
-
-//пример использования встроенных функций kint
-//use App\QueryBuilder;
-//$db = new QueryBuilder();
-//var_dump(get_class_methods($db));
-
-
-// Create new Plates instance
-//$templates = new League\Plates\Engine('../app/views');
-
-// Render a template
-//echo $templates->render('about', ['title' => 'Jonathan']);
-
+    require "../vendor/autoload.php";
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+  <?php
+  if(true) {
+      flash()->message('Hot!');
+  }
+  echo flash()->display('error');
+
+    flash()->message('Error!', 'error');
+//    echo flash()->display();
+
+    flash()->error('Error-type2!', 'error');
+//    echo flash()->display();
+
+    flash()->success('Success message!');
+//    echo flash()->display();
+
+    flash('something', 'error');
+
+  ?>
+</body>
+</html>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,9 +8,14 @@ class ComposerStaticInita19801140916e58f8557e2558f4bb693
 {
     public static $files = array (
         '3917c79c5052b270641b5a200963dbc2' => __DIR__ . '/..' . '/kint-php/kint/init.php',
+        '6157b075b923803e5ef157aeb43b83bd' => __DIR__ . '/..' . '/tamtamchik/simple-flash/src/function.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Tamtamchik\\SimpleFlash\\' => 23,
+        ),
         'L' => 
         array (
             'League\\Plates\\' => 14,
@@ -27,6 +32,10 @@ class ComposerStaticInita19801140916e58f8557e2558f4bb693
     );
 
     public static $prefixDirsPsr4 = array (
+        'Tamtamchik\\SimpleFlash\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tamtamchik/simple-flash/src',
+        ),
         'League\\Plates\\' => 
         array (
             0 => __DIR__ . '/..' . '/league/plates/src',
