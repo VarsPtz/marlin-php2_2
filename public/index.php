8882c39e3851d35312fb8f29ddc539b38b1e5cd4
Alexandr Varygin <otz-job@mail.ru>
2020-01-06 16:23:41 +0300

    start use the component nikic/fast-route

--- public/index.php
+++ public/index.php
@@ -3,41 +3,54 @@
     if( !session_id() ) @session_start();
 
     require "../vendor/autoload.php";
+
+    d($_SERVER);
+
+    $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+        $r->addRoute('GET', '/users', 'get_all_users_handler');
+        // {id} must be a number (\d+)
+        $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+        // The /{title} suffix is optional
+        $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+    });
+
+    // Fetch method and URI from somewhere
+    $httpMethod = $_SERVER['REQUEST_METHOD'];
+    $uri = $_SERVER['REQUEST_URI'];
+
+    // Strip query string (?foo=bar) and decode URI
+    if (false !== $pos = strpos($uri, '?')) {
+        $uri = substr($uri, 0, $pos);
+    }
+    $uri = rawurldecode($uri);
+
+    $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            // ... Page not found - 404 Not Found
+            break;
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $allowedMethods = $routeInfo[1];
+            // ... 405 Method Not Allowed
+            break;
+        case FastRoute\Dispatcher::FOUND:
+            $handler = $routeInfo[1];
+            $vars = $routeInfo[2];
+            // ... call $handler with $vars
+
+            // call_user_func('name_of_calling_function');
+            // or
+             call_user_func($handler);
+            break;
+    }
+
+    function name_of_calling_function() {
+        echo 123;
+    }
+
+    // or
+
+    function get_all_users_handler() {
+        echo 123;
+    }
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-</head>
-<body>
-  <?php
-  if(true) {
-      flash()->message('Hot!');
-  }
-//  echo flash()->display('error');
-  echo flash()->display();
-
-
-
-//
-    flash()->message('Error!', 'error');
-    echo flash()->display();
-
-    flash()->error('Error-type2!', 'error');
-    echo flash()->display();
-
-    flash()->success('Success message!');
-    echo flash()->display();
-
-    flash('something', 'error');
-    echo flash()->display();
-
-
-  ?>
-</body>
-</html>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInita19801140916e58f8557e2558f4bb693
 {
     public static $files = array (
         '3917c79c5052b270641b5a200963dbc2' => __DIR__ . '/..' . '/kint-php/kint/init.php',
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         '6157b075b923803e5ef157aeb43b83bd' => __DIR__ . '/..' . '/tamtamchik/simple-flash/src/function.php',
     );
 
@@ -23,6 +24,10 @@ class ComposerStaticInita19801140916e58f8557e2558f4bb693
         'K' => 
         array (
             'Kint\\' => 5,
+        ),
+        'F' => 
+        array (
+            'FastRoute\\' => 10,
         ),
         'A' => 
         array (
@@ -43,6 +48,10 @@ class ComposerStaticInita19801140916e58f8557e2558f4bb693
         'Kint\\' => 
         array (
             0 => __DIR__ . '/..' . '/kint-php/kint/src',
+        ),
+        'FastRoute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nikic/fast-route/src',
         ),
         'Aura\\SqlQuery\\' => 
         array (
