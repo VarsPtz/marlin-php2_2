beea7cac2f021dc717b81683b1145330a7530dc0
Alexandr Varygin <otz-job@mail.ru>
2020-01-27 22:54:56 +0300

    lesson-13 Views t:4:13

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,24 +5,6 @@ namespace App\controllers;
 use App\QueryBuilder;
 use League\Plates\Engine;
 
-//$db = new QueryBuilder();
-//$posts = $db->getAll('posts');
-
-//$db->insert([
-//    'title' => 'new post from QueryFactory package'
-//],'posts');
-
-//$db->update([
-//    'title' => 'new post from QueryFactory package-2'
-//],23,'posts');
-
-//$db->delete(23,'posts');
-
-//$post = $db->getOne(22, 'posts');
-
-//var_dump($post);
-
-//lesson route-2
 class HomeController {
 
     private $templates;
@@ -33,32 +15,12 @@ class HomeController {
     }
 
     public function index($vars)
-
     {
-        // Create new Plates instance
-//        $templates = new Engine('../app/views');
-
-        // Render a template
         echo $this->templates->render('homepage', ['name' => 'Jonathan']);
-
-//        d($vars); exit();
-//        $db = new QueryBuilder();
-
-//        $db->update([
-//            'title' => 'new post from QueryFactory package2'
-//        ], 2, 'posts');
-
-
     }
 
     public function about($vars)
     {
-//        d($vars);
-
-        // Create new Plates instance
-//        $templates = new Engine('../app/views');
-
-        // Render a template
         echo $this->templates->render('about', ['name' => 'This page about Jonathan']);
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,29 +4,10 @@
 
     require "../vendor/autoload.php";
 
-//    d($_SERVER);
-
     $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-//        $r->addRoute('GET', '/users', 'get_all_users_handler');
-
-        //controller name -> HomeController,  method -> index
-//        $r->addRoute('GET', '/users', ['HomeController', 'index']);
-//        $r->addRoute('GET', '/users', ['App\controllers\HomeController', 'index']);
-
         // lesson-14 Views
         $r->addRoute('GET', '/home', ['App\controllers\HomeController', 'index']);
         $r->addRoute('GET', '/about', ['App\controllers\HomeController', 'about']);
-
-
-        // {id} must be a number (\d+)
-//        $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
-//        $r->addRoute('GET', '/user/{id:\d+}', ['App\controllers\HomeController', 'index']);
-
-        //users/5/company/classes/school/4
-//        $r->addRoute('GET', '/users/{id:\d+}/company/classes/{number:\d+}', ['App\controllers\HomeController', 'about']);
-
-        // The /{title} suffix is optional
-//        $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
     });
 
     // Fetch method and URI from somewhere
@@ -53,38 +34,9 @@
         case FastRoute\Dispatcher::FOUND:
             $handler = $routeInfo[1];
             $vars = $routeInfo[2];
-//            d($handler, $vars);exit;
-//            $class = $handler[0];
-//            $method = $handler[1];
-
-            // ... call $handler with $vars
-
-            // call_user_func('name_of_calling_function');
-
-            // or
-//            call_user_func($handler, $vars);
-//            d($handler[0]);exit();
             $controller = new $handler[0];
-//            $controller->index(123);
-//            d($controller);exit();
             call_user_func([$controller, $handler[1]], $vars);
             break;
     }
-
-//    function name_of_calling_function() {
-//        echo 123;
-//    }
-
-    // or
-
-//    function get_all_users_handler($vars) {
-//        d($vars);
-//    }
-
-//    /user/1
-//    function get_user_handler($vars) {
-//        d($vars['id']);
-//    }
-
 
 ?>
